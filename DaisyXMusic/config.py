7a4b01bfb2af328eac3fc7d0ba8255e8990ecd15
looserakki <70113298+looserakki@users.noreply.github.com>
2021-08-02 16:25:42 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -9,7 +9,7 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "AQAAjg_nngvg5JUHI_tIjTIFl74Vgexp4PmWGP7iu9SwbWBjTlRzVXdpLyQtooGsI3NXl50JstWuu9VRr8oJT7Keh2giGTwsweIgPt-8-pZL1ik6sXNXatDbXIOlNe3mpMrPwqTiqwU9UgRXtZiuOsqUI5F86aIagXl03Pw-opGA6PzXd3mjFa-9sE_5kxI5DDtAvPRZ0Dshth744zKE0yi8r1yHfavLad7p-z27qj64K2kQZklTjbkjjRip3dR8vJZBqBqXHJpZaYG5K-pLrR0ZFh0Mx8UCx7aaaDL-Y_cdBcLqk3t8DbQGJZsqobjkpOxjTsU7fYzXGTOtSf5Bm1YvdCqCJAA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1909102195:AAEbJIvqPKShOjvJat9EFMuPm3Dc8DGfg34")
+BOT_TOKEN = getenv("BOT_TOKEN", "1909102195:AAGeKwinn5v-UqNClO_TjZNciWsu82_H5ZA")
 BOT_NAME = getenv("BOT_NAME", "SHERSID")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
