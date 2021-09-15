85aa12e85951314c0c3a172a5a26890fef0fd204
looserakki <70113298+looserakki@users.noreply.github.com>
2021-09-15 06:12:15 +0530

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -4,15 +4,15 @@ import os
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "AQCEZW7enth3VEDnHwRNKa8AH_ur2fFQw5iP5J5ViKWofb7fmDBLlKlUUfanzQP9RuPWG_oZMHg4PfYFEHoA3d8ySdyBjFOlIuXIoGp9XqIXSDYZWyERMBpo9mit6pQWqYx4QylHJiYY1mb4FWgUtceHGzD2DDvHxd4HwnTmQb3Y1bagCLJZ6_MgmQa_44hv3qdPAEVgd5rNqkniFmm8JRFtUUCrt0eDPEpu5qI0TVdTHmIiz-B6Bi_aKDs4mAgFhUTt93dndTCnA7h0QbBX22fLzC8pN_hak2H1bHkbffUaz6q1pJ1xLzLS3ScUlSU_Pso3Mfu65rlyg_OgF4FzLEvHdeEnewA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1942268270:AAHtPGD5rKaK_gFTL9OQasqQRHGUrAZr5aA)
+BOT_TOKEN = getenv("BOT_TOKEN", "1942268270:AAHtPGD5rKaK_gFTL9OQasqQRHGUrAZr5aA")
 BOT_NAME = getenv("BOT_NAME", "MUSICBOT")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DEECODEBOTS")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
 admins = {}
 API_ID = int(getenv("API_ID", "7343230"))
-API_HASH = getenv("API_HASH", "66ef09b43e03eeafb6a215df1948285a)
-BOT_USERNAME = getenv("BOT_USERNAME", "Decode)
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "xdAssistant)
+API_HASH = getenv("API_HASH", "66ef09b43e03eeafb6a215df1948285a")
+BOT_USERNAME = getenv("BOT_USERNAME", "Decode")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "xdAssistant")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DeCodeSupport")
 PROJECT_NAME = getenv("PROJECT_NAME", "PatriciaXmusic")
 SOURCE_CODE = getenv("SOURCE_CODE", "t.me/puroXpower")
